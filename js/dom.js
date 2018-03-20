e4bd8d69a79996f7ae60e9267f50f802e4ab270a
Nue <undefinedhoujuunue@gmail.com>
2018-03-20 18:41:55 -0400

    day1

---
+++ js/dom.js
@@ -0,0 +1,38 @@
+matchThree.dom = (function(){
+	function $(path,parent){
+		parent = parent || document;
+		return parent.querySelectorAll(path);
+	}
+
+	function hasClass(el,clsName){
+		var regex = new RegExp("(^|\\s)" + clsName+"(\\s|$)");
+		return regex.text(el.className);
+	}
+
+	function addClass(el,clsName){
+		if(!hasClass(ek,clsName)){
+			el.className += " " + clsName;
+		}
+	}
+
+	function bind(el,event,handler){
+		if(typeof el == "string"){
+			el = $(el)[0];
+		}
+
+		el.addEventListener(event,handler,false);
+	}
+
+	function removeClass(el,clsName){
+		var regex = new RegExp("(^|\\s)" + clsName+"(\\s|$)");
+		el.className = el.className.replace(regex," ");
+	}
+
+	return{
+		$ : $,
+		hasClass : hasClass,
+		removeClass : removeClass,
+		bind : bind
+	};
+
+})();
---
+++ js/matchThree.js
@@ -0,0 +1,67 @@
+var matchThree = (function(){
+
+	var scriptQueue = [];
+	var numResourcesLoaded = 0;
+	var numResources = 0;
+	var executeRunning = false;
+
+	function executeScriptQueue(){
+		var next = scriptQueue[0];
+		var first, script; 
+
+		if(next && next.loaded){
+			executeRunning = true;
+			scriptQueue.shift();
+			first = document.getElementsByTagName("script")[0];
+			script = document.createElement("script");
+			script.onLoad = function(){
+				if(next.callback){
+					next.callback();
+				}
+				executeScriptQueue();
+			};
+			script.src = next.src;
+			first.parentNode.insertBefore(script, first);
+		} else {
+			executeRunning = false;
+		}
+	}
+
+	function load(src,callback){
+		var image, queueEntry;
+
+		numResources++;
+
+		queueEntry = {
+			src : src,
+			callback : callback,
+			loaded : false
+		}
+
+		scriptQueue.push(queueEntry);
+
+		image = new Image();
+		image.onLoad = image.onerror = function(){
+			numResources++;
+			queueEntry.loaded = true;
+			if(!executeRunning){
+				executeScriptQueue();
+			}
+		};
+		image.src = src;
+
+	}
+
+	function setup(){
+		console.log("Success!")
+	}
+
+	function showScreen(screenId){
+		var dom = matchThree.dom;
+	}
+
+	return {
+		load : load,
+		setup : setup
+	};
+})();
